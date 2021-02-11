01eecbe5ec03786986d4bfecd57c20c2373fd050
Stephanie Mitchell <stephsorandom@gmail.com>
2021-02-11 16:16:05 -0500

    iteration 1.3

---
+++ src/Components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Navbar(props) {
+    return (
+        <nav class="navbar navbar-dark bg-primary mb-3">
+        <div class="container">
+          <a class="navbar-brand" href="/">WikiCountries</a>
+        </div>
+      </nav>
+    );
+}
+
+export default Navbar;
